531e477f3ae89a92dfeecca658535fbb6c924abd
mykelcodex <michealoke39@yahoo.com>
2020-07-17 12:28:55 +0100

    First Commit

---
+++ nuxt.config.js
@@ -0,0 +1,117 @@
+
+const env = require('dotenv').config()
+
+const client = require('./plugins/contentful')
+
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+ env: env.parsed,
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/png', href: '/images/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', href: '/images/favicon-16x16.png' },
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#FFC25C' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    { src: '~/node_modules/highlight.js/styles/hopscotch.css', lang: 'js' },
+    { src: '~/node_modules/highlight.js/styles/hopscotch.css', lang: 'css' },
+    { src: '~/node_modules/highlight.js/styles/hopscotch.css', lang: 'php' }
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '~/plugins/contentful',
+    '~/plugins/day',
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/svg',
+    '@nuxtjs/dotenv',
+    '@nuxtjs/markdownit',
+    ['@nuxtjs/sitemap',{
+      path: '/sitemap.xml',
+      hostname: 'https://omike.me',
+      generate: true,
+      routes:[
+        {
+          url: '/',
+          changefreq: 'daily',
+          priority: 1,
+          lastmod: '2020-06-30T13:30:00.000Z'
+        },
+        {
+          url: '/blog',
+          changefreq: 'daily',
+          priority: 1,
+          lastmod: '2020-06-30T13:30:00.000Z'
+        }
+      ]
+    }]
+  ],
+
+  markdownit:{
+    injected: true,
+    use: [
+      'markdown-it-highlightjs'
+    ]
+  },
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+
+  generate: {
+    routes() {
+      return client.getEntries({ content_type : 'post' })
+      .then(entries =>{
+        return entries.items.map(entry=>{
+          return {
+            route: entry.fields.slug,
+            payload: entry
+          }
+        })
+      })
+    }
+   },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
